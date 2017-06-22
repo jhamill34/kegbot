33125b9398de23fe22266f707ce42ee1f5530fdb
Joshua L. Rasmussen <xlr8runner@gmail.com>
2017-06-21 22:27:27 -0700

    Move DB stuff to environment variables

--- web/views/session.py
+++ web/views/session.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-engine_string = 'postgresql://docker:mysecretpassword@db:5432/kegbot'
+engine_string = os.environ.get('SQL_STRING')
 engine = create_engine(engine_string)
 
 Base.metadata.create_all(engine)
